ee3963337c5065f889f0aa4d7a1daa18891b87e1
hrJin9 <bushyerin97@gmail.com>
2023-07-04 23:55:35 +0900

    [PGS] - 올바른 괄호

---
+++ 2주차/hyerin/올바른 괄호/올바른 괄호.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s  = sc.nextLine();
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s) { //괄호문제
+        boolean answer = true;
+
+        char[] cArray = s.toCharArray();
+        Stack charStack = new Stack<Character>();
+
+        for(char c : cArray){
+            if(c == '('){
+                charStack.push(c);
+            } else {
+                if(!charStack.isEmpty())
+                    charStack.pop();
+                else
+                    answer = false;
+            }
+        }
+
+        if(!charStack.isEmpty())
+            answer = false;
+
+        return answer;
+    }
+
+}
